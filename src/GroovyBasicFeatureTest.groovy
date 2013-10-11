dafbb42108c93520827e128e3dffd7ade5d8f906
Armin Kling <armin@pegasus.site>
2013-10-12 00:02:17 +0200

    First commit

---
+++ src/GroovyBasicFeatureTest.groovy
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: armin
+ * Date: 11.10.13
+ * Time: 23:44
+ * To change this template use File | Settings | File Templates.
+ */
+class GroovyBasicFeatureTest {
+}
